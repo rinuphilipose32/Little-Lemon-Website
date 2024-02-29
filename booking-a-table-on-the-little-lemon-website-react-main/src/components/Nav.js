f76e29d61138d3fd2517b26f619d2b4c825e8693
rinuphilipose32 <140627130+rinuphilipose32@users.noreply.github.com>
2024-02-29 13:22:29 +0530

    uploads

---
+++ booking-a-table-on-the-little-lemon-website-react-main/src/components/Nav.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Outlet, Link } from "react-router-dom";
+import HeaderLogo from '../images/logo/header-logo.png'
+import navStyle from './components.module.css/Nav.module.css'
+
+const Nav = () => {
+    return (
+        <div className={navStyle.nav}>
+            <Link to="/booking-a-table-on-the-little-lemon-website-react">
+                <img src={HeaderLogo} alt="logo" />
+            </Link>
+
+            <div className={navStyle.headerLogoLine}></div>
+
+            <div className={navStyle.navList}>
+                <ul>
+                    <li>
+                        <Link to="/booking-a-table-on-the-little-lemon-website-react">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/booking-a-table-on-the-little-lemon-website-react/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/booking-a-table-on-the-little-lemon-website-react/menu">Menu</Link>
+                    </li>
+                    <li>
+                        <Link to="/booking-a-table-on-the-little-lemon-website-react/reservations">Reservations</Link>
+                    </li>
+                    <li>
+                        <Link to="/booking-a-table-on-the-little-lemon-website-react/order-online">Order online</Link>
+                    </li>
+                    <li>
+                        <Link to="/booking-a-table-on-the-little-lemon-website-react/login">Login</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
